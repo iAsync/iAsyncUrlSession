55a8dd9cc65d6f220e5eea7c44920dcd38f6d7b8
dodikk <dodikk88.reg@gmail.com>
2013-12-03 18:22:18 +0200

    Added missing header

--- iAsyncUrlSession/iAsyncUrlSession.h
+++ iAsyncUrlSession/iAsyncUrlSession.h
@@ -1,6 +1,7 @@
 
 #import <iAsyncUrlSession/JNUrlSessionOperationBuilder.h>
 
+#import <iAsyncUrlSession/JNDownloadProgressInfoPOD.h>
 #import <iAsyncUrlSession/JNDownloadProgressInfo.h>
 #import <iAsyncUrlSession/JNUrlSessionConnectionCallbacks.h>
 #import <iAsyncUrlSession/JNUrlSessionConnectionBlocks.h>
