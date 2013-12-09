6e14393c46d38469dbfef28d18863530af58f993
dodikk <dodikk88.reg@gmail.com>
2013-12-09 15:40:54 +0200

    Discarding untrusted certificates by default

--- iAsyncUrlSession/iAsyncUrlSessionDefines.h
+++ iAsyncUrlSession/iAsyncUrlSessionDefines.h
@@ -1,6 +1,6 @@
 #ifndef iAsyncUrlSession_iAsyncUrlSessionDefines_h
 #define iAsyncUrlSession_iAsyncUrlSessionDefines_h
 
-#define TRUST_ALL_CERTIFICATES_BY_DEFAULT 1
+#define TRUST_ALL_CERTIFICATES_BY_DEFAULT 0
 
 #endif
