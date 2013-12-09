4646de4b13455cd2d8aee19612d465c4804b73db
dodikk <dodikk88.reg@gmail.com>
2013-12-09 15:42:58 +0200

    Added a method to detect certificate error

--- iAsyncUrlSession/Error/JNUrlSessionConnectionError.h
+++ iAsyncUrlSession/Error/JNUrlSessionConnectionError.h
@@ -1,4 +1,7 @@
 #import <iAsyncUrlSession/Error/JNAbstractUrlSessionError.h>
 
 @interface JNUrlSessionConnectionError : JNAbstractUrlSessionError
+
+-(BOOL)isUntrustedCertificateError;
+
 @end
--- iAsyncUrlSession/Error/JNUrlSessionConnectionError.m
+++ iAsyncUrlSession/Error/JNUrlSessionConnectionError.m
@@ -1,4 +1,14 @@
 #import "JNUrlSessionConnectionError.h"
 
 @implementation JNUrlSessionConnectionError
+
+-(BOOL)isUntrustedCertificateError
+{
+    BOOL isNetworkError = [ self.underlyingError.domain isEqualToString: @"NSURLErrorDomain" ];
+    BOOL isBadCertificateError = ( self.underlyingError.code == -1202 );
+    
+    
+    return isNetworkError && isBadCertificateError;
+}
+
 @end
